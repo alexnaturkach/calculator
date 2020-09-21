7f4028b9903c1852f520c7aa49b6a9dcfdfc0dc7
Oleksandr Naturkach <oleksandrnaturkach@gmail.com>
2020-09-21 16:37:22 +0300

    Finished all the logic and features

--- test.js
+++ test.js
@@ -17,24 +17,48 @@ function equals() {
     } else if (currentOperator === 'multiply') {
       result = firstArgument * secondArgument;
     } else if (currentOperator === 'divide') {
-      result = firstArgument / secondArgument;
+      if (secondArgument === 0) {
+        reset();
+        display.innerText = 'Error';
+        return;
+      } else {
+        result = firstArgument / secondArgument;
+      }
     }
-    firstArgument = result; //saving the result of operation as a first argument
-    calcDisplay = '';
-    display.innerText = result.toString().replace(".", ",");
+    if (firstArgument.toString().length > 5) {
+      display.style.fontSize = "1.9em";
+    }
+    if (result.toString().length > 8) {
+      firstArgument = result; //saving the result of operation as a first argument
+      calcDisplay = '';
+      display.innerText = result.toExponential(5).toLocaleString('en-US').replace(/,/g, " ").replace('.', ',').replace('+', '');
+    } else {
+      firstArgument = result; //saving the result of operation as a first argument
+      calcDisplay = '';
+      display.innerText = result.toLocaleString('en-US').replace(/,/g, " ").replace('.', ',');
+    }
   }
+
 }
 
 function reset() {
-  result = 0;
+  result = '';
   firstArgument = 0;
   secondArgument = 0;
-  lastOperator = '';
+  currentOperator = '';
   calcDisplay = '';
   display.innerText = '0';
+  display.style.fontSize = "2.8em";
 }
+
+
 //click listeners
 document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('equals')) {
+    equals();
+    //firstArgument = result;
+    currentOperator = '';
+  }
   for (i = 0; i < allOperators.length; i++) { //remove highlight from the active operator
     if (allOperators[i].classList.contains('active')) {
       allOperators[i].classList.remove('active');
@@ -45,12 +69,22 @@ document.addEventListener('click', (e) => {
       //if display is showing the result of operation already, just reset the display screen
       display.innerText = '';
     }
-    calcDisplay = calcDisplay + e.target.innerText;
-    display.innerText = calcDisplay;
+    if (calcDisplay.length > 5) {
+      display.style.fontSize = "1.9em";
+    }
+    if (calcDisplay.length < 10 & calcDisplay.includes('.')) { //limit amount of characters that can be entered in display
+      calcDisplay = calcDisplay + e.target.innerText;
+      display.innerText = calcDisplay.toLocaleString('en-US').replace(/,/g, " ").replace(".", ","); //format the output in Display
+    } else if (calcDisplay.length < 9) {
+      calcDisplay = calcDisplay + e.target.innerText;
+      display.innerText = Number(calcDisplay).toLocaleString('en-US').replace(/,/g, " ").replace(".", ",");
+    }
   }
 
+
+
   if (e.target.classList.contains('operators')) {
-      //if clicked on operator and arguments are not empty, operate the arguments with the last selected operator
+    //if clicked on operator and arguments are not empty, operate the arguments with the last selected operator
     if ((currentOperator !== '') & (calcDisplay !== '')) {
       equals();
     };
@@ -60,11 +94,57 @@ document.addEventListener('click', (e) => {
         currentOperator = operatorList[i];
       }
     }
-    if ((calcDisplay !== '') & (firstArgument !== result)) {
-        firstArgument = parseFloat(calcDisplay.replace(",", "."));
-        calcDisplay = ''; //reset the Display value after saving it as a first argument
-      }
-          e.target.classList.add('active');
+    if (calcDisplay !== '') { //if display not empty, save it's value as a first argument
+      firstArgument = parseFloat(calcDisplay.replace(",", "."));
+      calcDisplay = ''; //reset the Display value after saving it as a first argument
     }
+    e.target.classList.add('active');
+  }
+  if (e.target.classList.contains('comma') & !(calcDisplay.toString().includes('.'))) {
+    if (calcDisplay === '') {
+      calcDisplay = calcDisplay + '0.';
+      display.innerText = calcDisplay.toLocaleString('en-US').replace(/,/g, " ").replace(".", ",");
+    } else {
+      calcDisplay = calcDisplay + '.';
+      display.innerText = calcDisplay.toLocaleString('en-US').replace(/,/g, " ").replace(".", ",");
+    }
+  }
 
+})
+
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('percentage') & currentOperator !== 'percent') {
+    if (currentOperator === '' & calcDisplay !== '') {
+      result = parseFloat(calcDisplay.replace(",", "."));
+      result = result / 100;
+      display.innerText = result.toLocaleString('en-US').replace(/,/g, " ").replace(".", ",");
+      currentOperator = 'percent';
+    } else {
+      equals();
+      if (result !== undefined) {
+        result = result / 100;
+        display.innerText = result.toLocaleString('en-US').replace(/,/g, " ").replace(".", ",");
+        currentOperator = 'percent';
+      }
+    }
+  }
+
+})
+
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('negativeSwitch')) {
+    if (calcDisplay === '') {
+      if (result.toString().includes('-')) {
+        result = result - result - result;
+      } else {
+        result = result - result - result;
+      }
+      calcDisplay = result + '';
+    } else if (calcDisplay[0] !== '-') {
+      calcDisplay = '-' + calcDisplay;
+    } else {
+      calcDisplay = calcDisplay.substring(1, calcDisplay[calcDisplay.length]);
+    }
+    display.innerText = calcDisplay;
+  }
 })
